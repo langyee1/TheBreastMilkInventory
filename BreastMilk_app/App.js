bc4730e0954cce8c117dc39627cfc38d068e05a5
Angelica <angelica.yee57@gmail.com>
2023-08-03 19:17:49 -0500

    completed push

--- BreastMilk_app/App.js
+++ BreastMilk_app/App.js
@@ -1,13 +1,15 @@
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { HomeStack } from './navigation/stack';
+//import { HomeStack } from './navigation/stack';
 import { StyleSheet } from 'react-native';
+import { MyDrawer } from './navigation/drawer';
+import { HomeStack } from './navigation/stack';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <NavigationContainer styles = {styles.container}>
-      <HomeStack>
-
-      </HomeStack>
+      <MyDrawer></MyDrawer>
     </NavigationContainer>
   );
 }
