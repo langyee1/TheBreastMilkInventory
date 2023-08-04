9b8bebc23732c74d10781ccedd72e353e44569bd
Angelica <angelica.yee57@gmail.com>
2023-08-04 14:19:54 -0500

    added all the remaining json files

--- BreastMilk_app/screens/ProperStorageScreen.js
+++ BreastMilk_app/screens/ProperStorageScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 const ProperStorageScreen = () => {
     const navigation = useNavigation();
@@ -18,6 +19,23 @@ const ProperStorageScreen = () => {
     const styles = StyleSheet.create({
     screen: {
         padding: 10,
+        headerTintColor: '#2d2d2d',
+        headerStyle: {
+            backgroundColor: 'lightyellow',
+            alignItems: 'flex-end', 
+            justifyContent: 'space-between', 
+        },
+        headerRight: () => (
+            <Ionicons
+                name = 'menu'
+                size= {32}
+                color = 'black'
+                onPress={()=>nav.toggleDrawer()}
+            />
+        ),
+        headerLeft: () => (
+            <Text style={{color:'black', fontSize: 30, paddingLeft: 5}}>Logo</Text>
+        )
     },
     homeButton: {
         fontSize: 16,
