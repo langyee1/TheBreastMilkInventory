5ab91e20f22a0c1e7e4e0eb6d1c8e62c6b34229f
Angelica <angelica.yee57@gmail.com>
2023-08-04 11:46:39 -0500

    modified jar image and made the about screen simple again wo functionality

--- BreastMilk_app/screens/AboutScreen.js
+++ BreastMilk_app/screens/AboutScreen.js
@@ -1,19 +1,7 @@
-import { useLayoutEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import {HeaderBackButton} from "@react-navigation/stack";
+
 
 const AboutScreen = () => {
-    
-    const navigation = useNavigation()
-    useLayoutEffect(()=>{
-        navigation.setOptions({
-            headerTitle: 'new title',
-            headerLeft: () => (
-                <HeaderBackButton label="Previous" onPress={() => navigation.goBack()} />
-            )
-        })
-    })
     return (
         <View style={styles.screen}>
             <Text>This is the about screen</Text>
