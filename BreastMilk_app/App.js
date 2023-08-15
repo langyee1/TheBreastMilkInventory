1c5d81966d13df7893d2197d37da0f0a5789dfcb
Angelica <angelica.yee57@gmail.com>
2023-08-15 00:34:04 -0500

    added the intro swipe

--- BreastMilk_app/App.js
+++ BreastMilk_app/App.js
@@ -1,11 +1,45 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-//import { HomeStack } from './navigation/stack';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { MyDrawer } from './navigation/drawer';
-import { HomeStack } from './navigation/stack';
+import Swiper from 'react-native-swiper';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [completedOnboarding, setCompletedOnboarding] = useState(false);
+  if (!completedOnboarding) {
+    return (
+      <Swiper
+        style={styles.wrapper} 
+        showsButtons={false}
+        loop={false} // Disable looping of slides
+        onIndexChanged={(index) => {
+          if (index === 4) {
+            setCompletedOnboarding(true);
+          }
+        }}
+      >
+        <View style={styles.slide}>
+          {/*<Image source={require('./path_to_image1.png')} style={styles.image} />*/}
+          <Text>Page 1</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text>Page 2</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text>Page 3</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text>Page 4</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text>Page 5</Text>
+          <Button title="Get Started" onPress={() => setCompletedOnboarding(true)} />
+        </View>
+      </Swiper>
+    );
+  }
   return (
     <NavigationContainer styles = {styles.container}>
       <MyDrawer></MyDrawer>
@@ -18,6 +52,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'red', 
   },
+  wrapper: {},
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  }
 });
 
 
